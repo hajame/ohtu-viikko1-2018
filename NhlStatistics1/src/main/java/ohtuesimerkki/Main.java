e0ff0018740aa62d8c5b568557139c1261d60b66
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-11-06 17:32:10 +0200

    w2 checkstyle

--- NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
+++ NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.println("Philadelphia Flyers");
         for (Player player : stats.team("PHI") ) {
             System.out.println( player );
+            for (int i=0; i < 2; i++) {
+              System.out.println("Nested!");
+            }
         }
         
         System.out.println("");
@@ -14,6 +17,11 @@ public class Main {
         System.out.println("Top scorers");
         for (Player player : stats.topScorers(10) ) {
             System.out.println( player );
+            if(true) {
+                if (true) {
+                    System.out.println("True!");
+                }
+            }
         }        
     }
 }
