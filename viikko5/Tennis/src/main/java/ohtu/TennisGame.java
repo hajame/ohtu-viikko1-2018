9422e869a9abd4b2434ee1d59e677e05bf8fd512
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-11-27 16:32:08 +0200

    viikko5 tehtävä2

--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,80 +1,76 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private int p1Score = 0;
+    private int p2Score = 0;
+    private final String player1;
+    private final String player2;
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = player1Name;
+        this.player2 = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1)) {
+            p1Score += 1;
+        } else {
+            p2Score += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (p1Score == p2Score) {
+            return evenScore();
+        } else if (p1Score >= 4 || p2Score >= 4) {
+            return resolveEndGame();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return resolveScore();
+    }
+
+    private String resolveScore() {
+        return printScore(p1Score)+"-"+printScore(p2Score);
+    }
+
+    private String printScore(int playerScore) {
+        switch (playerScore) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";                
+            default:
+                return "Forty";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private String resolveEndGame() {
+        int scoreDifference = p1Score - p2Score;
+        if (scoreDifference == 1) {
+            return "Advantage player1";
+        } else if (scoreDifference == -1) {
+            return "Advantage player2";
+        } else if (scoreDifference >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
-        return score;
+    }
+
+    private String evenScore() {
+        switch (p1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
     }
 }
